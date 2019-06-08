76911e4270a10a4e3a6e4950668e4b7cdbc655a9
Matt Jenkins <matt@majenko.co.uk>
2019-06-08 20:30:42 +0100

    Add Gamma programming interface bootloader

--- bootloaders/configs/majenko.h
+++ bootloaders/configs/majenko.h
@@ -13,6 +13,7 @@
 #define     prodLennyPlus               0x0006
 #define     prodPKRelay                 0x0007
 #define     prodGamma                   0x0008
+#define     prodGammaUSB                0x0009
 #define     prodModProg                 0x1001
 #define     prodFXBase                  0x1002
 #define     prodMotionTracker           0x1003
@@ -989,7 +990,7 @@
 // LIBUS Boards
 
 
-#if defined(_BOARD_MAJENKO_LIBUS_SPINNER_) || defined(_BOARD_MAJENKO_LIBUS_GENERAL_) || defined(_BOARD_MAJENKO_LIBUS_STRIP_) || defined(_BOARD_MAJENKO_LIBUS_DUALLED_)
+#if defined(_BOARD_MAJENKO_LIBUS_SPINNER_) || defined(_BOARD_MAJENKO_LIBUS_GENERAL_) || defined(_BOARD_MAJENKO_LIBUS_STRIP_) || defined(_BOARD_MAJENKO_LIBUS_DUALLED_) 
 #define _CONFIG_VALID_
 
 #if defined(PUT_CONFIG_BITS_HERE)
@@ -1148,6 +1149,88 @@
     #define FLASH_PAGE_SIZE             0x4000                      // 16K
 #endif
 
+#if defined(_BOARD_MAJENKO_GAMMA_USB_)
+#define _CONFIG_VALID_
+
+#if defined(PUT_CONFIG_BITS_HERE)
+
+    //* Oscillator Settings
+    #pragma config FNOSC    = PRIPLL                                // Oscillator selection
+    #pragma config POSCMOD  = EC                                    // Primary oscillator mode
+    #pragma config FPLLIDIV = DIV_2                                 // PLL input divider
+    #pragma config FPLLMUL  = MUL_21                                // PLL multiplier
+    #pragma config FPLLODIV = DIV_2                                 // PLL output divider
+    #pragma config FPBDIV   = DIV_1                                 // Peripheral bus clock divider
+    #pragma config FSOSCEN  = OFF                                   // Secondary oscillator enable
+
+    //* Clock control settings
+    #pragma config IESO     = OFF                                   // Internal/external clock switchover
+    #pragma config FCKSM    = CSECME                                // Clock switching (CSx)/Clock monitor (CMx)
+    #pragma config OSCIOFNC = OFF                                   // Clock output on OSCO pin enable
+
+    //* USB Settings
+    #pragma config UPLLEN   = ON                                    // USB PLL enable
+    #pragma config UPLLIDIV = DIV_2                                 // USB PLL input divider
+    #pragma config FVBUSONIO = OFF                                  // Make VBUSON a GPIO pin
+    #pragma config FUSBIDIO = OFF                                   // Controlled by port function
+
+    //* Other Peripheral Device settings
+    #pragma config FWDTEN   = OFF                                   // Watchdog timer enable
+    #pragma config WDTPS    = PS1024                                // Watchdog timer postscaler
+    #pragma config WINDIS   = OFF
+    #pragma config JTAGEN   = OFF                                   // JTAG port disabled
+
+    //* Code Protection settings
+    #pragma config CP       = OFF                                   // Code protection
+    #pragma config BWP      = OFF                                   // Boot flash write protect
+    #pragma config PWP      = OFF                                   // Program flash write protect
+
+    //*    Debug settings
+    #pragma config ICESEL   = ICS_PGx3                              // ICE/ICD Comm Channel Select
+    //#pragma config DEBUG    = ON                                  // DO NOT SET THIS CONFIG BIT, it will break debugging
+
+    #pragma config PMDL1WAY = OFF                                   // Allow multiple reconfigurations
+    #pragma config IOL1WAY  = OFF                                   // Allow multiple reconfigurations
+#endif
+
+    #define CAPABILITIES    ( blCapUSBSerialNumber | blCapBootLED | blCapDownloadLED | blCapSplitFlashBootloader | blCapUSBInterface | blCapProgramButton | blCapVirtualProgramButton | CAPCOMMON)
+
+    // BTN / LED sense
+    #define LedOn       High
+    #define BntOn       Low
+
+    // Boot LED
+    #define BLedLat     B
+    #define BLedBit     4
+
+    // Boot LED
+    #define DLedLat     A
+    #define DLedBit     4
+
+    #define USBManufacturerLen 40
+    #define USBManufacturer 'M',0,'a',0,'j',0,'e',0,'n',0,'k',0,'o',0,' ',0,'T',0,'e',0,'c',0,'h',0,'n',0,'o',0,'l',0,'o',0,'g',0,'i',0,'e',0,'s',0
+    #define USBProductLen 26
+    #define USBProduct 'P',0,'r',0,'o',0,'j',0,'e',0,'c',0,'t',0,' ',0,'G',0,'a',0,'m',0,'m',0,'a',0
+
+
+    // Virtual program button
+    #define VPBntLat    B
+    #define VPBntBit    2
+
+    // Program button
+    #define PBntPort    B
+    #define PBntBit     2
+    #define PBntPu      High
+
+    #define _CPU_NAME_                  "32MX270F256B"
+    #define VEND                        vendMajenko
+    #define PROD                        prodGammaUSB
+    #define F_CPU                       48000000UL
+    #define F_PBUS                      F_CPU
+
+    #define FLASH_BYTES                 (0x40000-0x1000)            // Leave room 4 pages (for bootloader!)
+    #define FLASH_PAGE_SIZE             1024                                // In bytes
+#endif
 
 
 //
